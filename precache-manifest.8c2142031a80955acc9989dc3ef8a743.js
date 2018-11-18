e54e49dfcc6d544e6f0f963d59f5357276459b61
Renato Oda <renato.oda2@gmail.com>
2018-11-18 21:58:17 -0200

    Updates

---
+++ precache-manifest.8c2142031a80955acc9989dc3ef8a743.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a6f9ed34dce4cc5eb61f12cb9e68ed7",
+    "url": "/react-kanban-list/static/media/ionicons.0a6f9ed3.eot"
+  },
+  {
+    "revision": "0781fed5694d0063fac6",
+    "url": "/react-kanban-list/static/css/main.ea42dfa2.chunk.css"
+  },
+  {
+    "revision": "582e81d61e0acaf4ace04a43ba18f2f5",
+    "url": "/react-kanban-list/static/media/ionicons.582e81d6.svg"
+  },
+  {
+    "revision": "d2fff5b62d7189d91ded",
+    "url": "/react-kanban-list/static/js/1.d2fff5b6.chunk.js"
+  },
+  {
+    "revision": "f7c3432784360a602e44",
+    "url": "/react-kanban-list/static/js/runtime~main.f7c34327.js"
+  },
+  {
+    "revision": "0781fed5694d0063fac6",
+    "url": "/react-kanban-list/static/js/main.0781fed5.chunk.js"
+  },
+  {
+    "revision": "96f1c901c087fb64019f7665f7f8aca6",
+    "url": "/react-kanban-list/static/media/ionicons.96f1c901.woff2"
+  },
+  {
+    "revision": "0d3af9ed87b47d5a3d414fbc5a70bca7",
+    "url": "/react-kanban-list/static/media/ionicons.0d3af9ed.woff"
+  },
+  {
+    "revision": "78ea68996cf2e7712c0b56f89f49dbf6",
+    "url": "/react-kanban-list/static/media/ionicons.78ea6899.ttf"
+  },
+  {
+    "revision": "d2fff5b62d7189d91ded",
+    "url": "/react-kanban-list/static/css/1.1f02ca35.chunk.css"
+  },
+  {
+    "revision": "664d3ca275b3c1d289acf5d65028b8e2",
+    "url": "/react-kanban-list/index.html"
+  }
+];
